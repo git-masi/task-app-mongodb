d02b89290bd0789b60d6fe2dba4ebd13068e45fa
Eric Masi <eric@gitmasi.com>
2020-03-13 11:45:22 -0400

    Add post routes for User and Task

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 const express = require('express');
 const chalk = require('chalk');
 
+require('./db/mongoose');
+const User = require('./models/user');
+const Task = require('./models/Task');
+
 const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/users', (req, res, next) => {
+  const user = new User(req.body);
+
+  user.save()
+    .then(() => {
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    })
+});
+
+app.post('/tasks', (req, res, next) => {
+  const task = new Task(req.body);
+
+  task.save()
+    .then(() => {
+      res.send(task);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
 
 app.use('/', (req, res, next) => {
   res.send('<h1>It\'s alive!</h1>')
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, (err) => {
-  if (!err) {
-    console.log('Server started:');
-    console.log(chalk.blue(`http://127.0.0.1:${PORT}`));
-  }
+app.listen(port, () => {
+  console.log('Server running');
+  console.log(chalk.blue(`http://127.0.0.1:${port}`));
 })
