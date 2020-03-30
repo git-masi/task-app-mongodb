3ac049b9790e0b7bd4c39554a85936dbeae9fdd1
Eric Masi <eric@gitmasi.com>
2020-03-30 07:09:12 -0400

    Set db to retry writes if they fail

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-const connectionURL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-iagm9.mongodb.net/${process.env.DB_DATABASENAME}`;
+const connectionURL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-iagm9.mongodb.net/${process.env.DB_DATABASENAME}?retryWrites=true`;
 
 mongoose.connect(connectionURL, {
   useNewUrlParser: true,
