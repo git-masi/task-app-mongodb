a707720ca659539c658a463e9296a03cd34246fa
Eric Masi <eric@gitmasi.com>
2020-03-31 11:47:09 -0400

    Refactor to remove dotenv config function calls
    There are many files that require dotenv. Rather than config dotenv in
    each file we can set a global config in the script.
    
    This works by using the require flag:
    -r dotenv/config
    
    In another update this config will specify a custom path

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,3 @@
-// ==================
-// DO NOT DELETE THIS
-require('dotenv').config();
-// ==================
-
 const mongoose = require('mongoose');
 
 const connectionURL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-iagm9.mongodb.net/${process.env.DB_DATABASENAME}?retryWrites=true`;
